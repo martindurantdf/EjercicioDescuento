faad5d526ae3b509e3b3b1849175fd92d3087966
martindurantdf <104039754+martindurantdf@users.noreply.github.com>
2022-05-03 10:07:10 -0300

    con enunciado

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-// Ejercicio descuento de 10% de un producto
+/*
+•Implemente un algoritmo que calcule y muestre
+por pantalla el precio final de un producto
+después de aplicarle un descuento
+• El precio inicial del producto es $450,50
+• El descuento a aplicar es del 10%. Recuerde que
+puede obtener el 10% de un valor multiplicado por 0,1
+• El precio y el descuento deben ser guardados en
+variables (no ingresados por teclado)
+ */
 let valorProducto: number;
 let valorDescuento: number;
 let precioFinal: number;
